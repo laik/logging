3ac28a7fe715c4b0c9e7f3283ba4c64651869473
laik <laik.lj@me.com>
2021-04-01 10:34:13 +0800

    first commit

---
+++ pkg/command/cmd_test.go
@@ -0,0 +1,25 @@
+package command
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func Test_run_command(t *testing.T) {
+	cmd := NewCmd()
+	pod1 := NewPod().SetName("echeor-api").SetNodeName("node1").SetOffset(1024).AddIp("127.0.0.1").AddIp("127.0.0.1")
+	pod2 := NewPod().SetName("echeor-api").SetNodeName("node1").SetOffset(1023).AddIp("127.0.0.1").AddIp("128.0.0.1")
+	cmdStr, err := cmd.SetOutput("kafka:test@10.200.100.200:9092").SetNs("test").SetRule("").AddPod(pod1).AddPod(pod2).Run()
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	expectedCmd := NewCmd()
+	if err := json.Unmarshal([]byte(cmdStr), &expectedCmd); err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	if len(expectedCmd.Pods) != 1 || expectedCmd.Op != RUN || expectedCmd.Output != "kafka:test@10.200.100.200:9092" || expectedCmd.Ns != "test" || expectedCmd.Pods[0].Offset != 1023 {
+		t.Fatal("unexpected test pod cmd")
+	}
+}
